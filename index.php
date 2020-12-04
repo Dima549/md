71533da114e494c30c89ac9009405cbbae4be31c
dima <nadyabogdanova2001@yandex.ru>
2020-12-04 22:16:14 +0300

    l3

--- index.php
+++ index.php
@@ -1,54 +1,21 @@
 <?php
 
-class A
+require 'core/EquationInterface.php';
+require 'core/LogInterface.php';
+require 'core/LogAbstract.php';
+
+require 'dimitriu/LinearEquation.php';
+require 'dimitriu/QuadraticEquation.php';
+require 'dimitriu/MyLog.php';
+
+$equation = new dimitriu\QuadraticEquation();
+try
 {
-    public function LinearEquation($a, $b)
-    {
-        if ($a == 0)
-        {
-            throw new Exception('Equation doesn`t exist');
-        }
-
-        return  $this->x = $b * (-1) / $a;
-    }
-
-    protected $x;
+    $result = $equation->solve(5, 14, 4);
+    $str = implode("; ", $result);
+    dimitriu\MyLog::Instance()::log($str);
 }
-
-class B extends A
+catch (Error $error)
 {
-
-    protected function Discriminant($a, $b, $c)
-    {
-        return $b * $b - 4 * $a * $c;
-    }
-
-    public function QuadraticEquation($a, $b, $c)
-    {
-        if ($a == 0)
-        {
-            return $this->LinearEquation($b, $c);
-        }
-
-        $d = $this->Discriminant($a, $b, $c);
-
-        if ($d == 0)
-        {
-            return $this->x = ($b * (-1)) / (2 * $a);
-        }
-
-        if ($d < 0)
-        {
-            throw new Exception('Equation doesn`t exist');
-        }
-
-        return $this->x = [($b * (-1) - sqrt($d)) / (2 * $a), ($b * (-1) + sqrt($d)) / (2 * $a)];
-    }
-
+    dimitriu\MyLog::Instance()::log($error->getMessage());
 }
-
-$equation1 = new A();
-$equation2 = new B();
-echo $equation1->LinearEquation(5, 6) . "\n";
-var_dump($equation2->QuadraticEquation(4.5, 12, 4));
-echo $equation2->QuadraticEquation(0, 4, 7) . "\n";
