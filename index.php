a24713579b487ded2d438d03459019838a1a3a3a
dima <nadyabogdanova2001@yandex.ru>
2020-12-04 22:11:40 +0300

    l2

--- index.php
+++ index.php
@@ -1,37 +1,54 @@
-<?hph
+<?php
 
 class A
 {
+    public function LinearEquation($a, $b)
+    {
+        if ($a == 0)
+        {
+            throw new Exception('Equation doesn`t exist');
+        }
+
+        return  $this->x = $b * (-1) / $a;
+    }
+
+    protected $x;
 }
 
 class B extends A
 {
-	public function __construct($a, $b)
-	{
-		$this->a = $a;
-		$this->b = $b;
-	}
-	
-	protected $a;
-	protected $b;
+
+    protected function Discriminant($a, $b, $c)
+    {
+        return $b * $b - 4 * $a * $c;
+    }
+
+    public function QuadraticEquation($a, $b, $c)
+    {
+        if ($a == 0)
+        {
+            return $this->LinearEquation($b, $c);
+        }
+
+        $d = $this->Discriminant($a, $b, $c);
+
+        if ($d == 0)
+        {
+            return $this->x = ($b * (-1)) / (2 * $a);
+        }
+
+        if ($d < 0)
+        {
+            throw new Exception('Equation doesn`t exist');
+        }
+
+        return $this->x = [($b * (-1) - sqrt($d)) / (2 * $a), ($b * (-1) + sqrt($d)) / (2 * $a)];
+    }
 
 }
 
-class C extends B
-{ 
-	public function __construct($a, $b, $c)
-	{
-		$this->c = $c;
-		parent::__construct($a, $b);
-	}
-	
-	protected $c;
-}
-
-$a1 = new A();
-$a2 = new A();
-$a3 = new A();
-$a4 = new A();
-$b5 = new B($a3, $a4);
-$c6 = new C($a1, $a2, $b5);
-		
+$equation1 = new A();
+$equation2 = new B();
+echo $equation1->LinearEquation(5, 6) . "\n";
+var_dump($equation2->QuadraticEquation(4.5, 12, 4));
+echo $equation2->QuadraticEquation(0, 4, 7) . "\n";
